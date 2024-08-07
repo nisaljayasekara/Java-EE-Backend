50cfb394ed4742059d9bfe702d8d2342967ac1af
nisaljayasekara <j.nmjayasekara@gmail.com>
2024-08-07 20:24:35 +0530

    BOFactory , DAOFactory updated

---
+++ src/main/java/com/example/demo/bo/BOFactory.java
@@ -0,0 +1,28 @@
+package com.example.demo.bo;
+
+import com.example.demo.bo.custom.impl.CustomerBOImpl;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory(){}
+
+    public static BOFactory getBoFactory(){
+        return (boFactory == null) ? boFactory = new BOFactory() : boFactory;
+    }
+
+    public enum BOTypes{
+        CUSTOMER_BO
+
+    }
+
+    public <T extends SuperBO> T getBO(BOTypes boTypes){
+        switch (boTypes) {
+            case CUSTOMER_BO:
+                return (T) new CustomerBOImpl();
+
+            default:
+                return null;
+        }
+        }
+}
